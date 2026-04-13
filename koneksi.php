a067caa6977d950a01fc27d145a1f44b44bcc801
HeavenlyD12 <zandrenputra@gmail.com>
2026-04-13 14:08:14 +0700

    tambah
    tambah

--- koneksi.php
+++ koneksi.php
@@ -10,4 +10,9 @@ if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 zandren padang
+
+ahahahahahahah
+hahahahaa
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum natus distinctio deleniti inventore saepe repudiandae pariatur enim quod quam? Accusamus nihil harum magnam facere dolore, animi temporibus itaque molestiae rem!
+
 ?>
